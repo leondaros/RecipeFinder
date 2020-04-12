e8b90f97c944c81caff170f51b5fac80e5e808cc
leondaros <leondaros@users.noreply.github.com>
2020-04-12 00:26:31 -0300

    format response from API

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const axios = require('axios');
 
 const app = express()
 app.get("/", async (req, res) => {
-    recipes = await getRecipes(req._parsedUrl.search)
-    res.send(recipes.data);
+    response = await getRecipes(req._parsedUrl.search)
+    recipes = formatResponse(response, req)
+    res.send(recipes);
 })
 
 getRecipes = (params) => {
     return axios.get("http://www.recipepuppy.com/api/"+params)
+            .then(res => res.data.results)
+}
+
+formatResponse = (recipes,params) => {
+    result = {}
+    result["recipes"] = recipes.map((recipe)=>{
+        return {
+            "title": recipe.title,
+            "ingredients": recipe.ingredients.split(", "),
+            "link": recipe.href,
+            "gif": ""
+        }
+    })
+    return result
 }
 
 app.listen(3000)
