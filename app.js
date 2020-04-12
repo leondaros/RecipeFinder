83602f869eb5b2af6456deec002eae4d3a4b6526
leondaros <leondaros@users.noreply.github.com>
2020-04-11 23:49:22 -0300

    search recipes using parameters

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const axios = require('axios');
 
 const app = express()
 app.get("/", async (req, res) => {
-    recipes = await axios.get("http://www.recipepuppy.com/api/?i=onions,garlic")
+    recipes = await getRecipes(req._parsedUrl.search)
     res.send(recipes.data);
 })
+
+getRecipes = (params) => {
+    return axios.get("http://www.recipepuppy.com/api/"+params)
+}
 
 app.listen(3000)
